620d5fc99de5364caa1fac3adcf2a5d2a1160e29
Georgios Giannakopoulos <54399720+georgegian2018@users.noreply.github.com>
2024-11-02 19:14:28 +0100

    Create annotation_service.dart

---
+++ annotation_service.dart
@@ -0,0 +1,30 @@
+// lib/services/annotation_service.dart
+import 'package:sqflite/sqflite.dart';
+import 'database_service.dart';
+import '../models/document.dart';
+
+class AnnotationService {
+  Future<void> addAnnotation(int documentId, Annotation annotation) async {
+    final db = await DatabaseService().database;
+    await db.insert(
+      'annotations',
+      {
+        'document_id': documentId,
+        'page': annotation.page,
+        'comment': annotation.comment,
+      },
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Annotation>> getAnnotations(int documentId) async {
+    final db = await DatabaseService().database;
+    final maps = await db.query(
+      'annotations',
+      where: 'document_id = ?',
+      whereArgs: [documentId],
+    );
+
+    return maps.map((map) => Annotation.fromMap(map)).toList();
+  }
+}
