6079d33cab81f3566ee7be5c79171c174ee43a95
Georgios Giannakopoulos <54399720+georgegian2018@users.noreply.github.com>
2024-11-02 19:20:11 +0100

    Update main.dart

--- main.dart
+++ main.dart
@@ -18,3 +18,25 @@ class ResearchLibraryApp extends StatelessWidget {
     );
   }
 }
+
+// lib/main.dart
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'screens/library_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(ResearchLibraryApp());
+}
+
+class ResearchLibraryApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Research Library',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LibraryScreen(),
+    );
+  }
+}
